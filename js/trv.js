5f028280ce1ad1adb7dd7c1d868f2eedba467b2d
calliedoctorsiu <callie.doctor@siu.edu>
2025-05-09 07:12:37 -0500

    Delete js directory

--- js/trv.js
+++
@@ -1,9 +0,0 @@
-const nav = document.querySelector('navbar');
-fetch('/nav_bar.html');
-then(res=>res.text());
-then(date=>{
-    nav.innerHTML=data;
-    const parser=new DOMParser();
-    const doc=parser.parseFromString(data,'text/html');
-    doc.querySelectorAll('script'.textContent);
-});
